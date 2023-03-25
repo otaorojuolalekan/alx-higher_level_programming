f5300e36078cdbc0fa13848b0e6baaef3efb11d1
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-03-25 23:26:29 +0100

    modified task solution by using List comprehension

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+# def divisible_by_2(my_list=[]):
+#     newlist = []
+#     for i in range(len(my_list)):
+#         if (my_list[i] % 2 == 0):
+#             newlist.append(True)
+#         else:
+#             newlist.append(False)
+#     return (newlist)
+
 def divisible_by_2(my_list=[]):
-    newlist = []
-    for i in range(len(my_list)):
-        if (my_list[i] % 2 == 0):
-            newlist.append(True)
-        else:
-            newlist.append(False)
-    return (newlist)
+    return [True if i % 2 == 0 else False for i in my_list]
