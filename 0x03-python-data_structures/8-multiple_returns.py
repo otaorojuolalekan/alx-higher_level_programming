0952ba7b4dd9cb9aee7d63443d01ae2e2f1b85ab
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-03-25 23:01:36 +0100

    tried alternate method to solve task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
-def multiple_returns(sentence):
+""" def multiple_returns(sentence):
     senlen = len(sentence)
     if senlen == 0:
         return (0, None)
-    return (senlen, sentence[0])
+    return (senlen, sentence[0]) """
+
+def multiple_returns(sentence):
+    slen = len(sentence)
+    return (slen, None if slen == 0 else sentence[0])
