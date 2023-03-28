770b7c8349cb7007a04790ca90d3cffbf7f52ffc
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-03-28 16:09:03 -0700

    Added scripts for 0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module Documentation.
+
+This is the first attempt to create a class in python.
+Don't fuss to much about it, this class does nothing
+"""
+
+
+class Square():
+    """This is an empty class that defines a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 0-square.py)
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
+Why?
+
+
+guillaume@ubuntu:~/0x06$ cat 1-main.py
+#!/usr/bin/python3
+Square = __import__('1-square').Square
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
+
+guillaume@ubuntu:~/0x06$ ./1-main.py
+<class '1-square.Square'>
+{'_Square__size': 3}
+'Square' object has no attribute 'size'
+'Square' object has no attribute '__size'
+guillaume@ubuntu:~/0x06$
+"""
+
+
+class Square():
+    """A Square with a Private Attribute of Size
+    Args:
+        size: Private Attribute of Square class.
+    Attributes:
+        size: Keep things private.
+    """
+    def __init__(self, size):
+        self.__size = size
