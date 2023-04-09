d688f6803b57df3e0bddb66f772d7899a1b36ef1
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-09 18:01:53 +0100

    fixed bug in task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -44,9 +44,9 @@ class Rectangle:
         """
         setter function for the private width field
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        if self.__size < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -61,6 +61,6 @@ class Rectangle:
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        if value <= 0:
+        if self.__height < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
