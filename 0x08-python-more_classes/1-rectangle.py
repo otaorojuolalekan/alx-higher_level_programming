f588db6f9ddd49e070c86227956a51b5b93d9b90
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-09 18:29:35 +0100

    otisumi o

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,15 +29,17 @@ class Rectangle:
     the module doc_string above.
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
+        """Get width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """set width of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if self.__width < 0:
@@ -46,10 +48,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """get height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """set height of the rectangle"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if self.__height < 0:
