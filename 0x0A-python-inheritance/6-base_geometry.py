b420028cc85da042b7cdec812a0a1f1f162bdcf5
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-14 08:11:28 +0100

    Added scripts for 0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains an empty geometry class
+"""
+
+
+class BaseGeometry:
+    """
+    the empty geometry class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
