c982f9ee69577149d09a24de3de5dbf1737c65d2
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-14 10:37:20 +0100

    Added scripts for 0x0A. Python - Inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,7 @@ Module containign scripts for task 3
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns True if obj is an instance or inherited 
+    Returns True if obj is an instance or inherited
     from a_class, else False
     """
     return (isinstance(obj, a_class))
