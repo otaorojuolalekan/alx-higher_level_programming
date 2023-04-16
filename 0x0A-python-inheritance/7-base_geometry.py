07a468b247e7658724880e8d033efcf17ae5b70f
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-16 18:55:03 +0100

    commit pending files

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,15 +8,11 @@ class BaseGeometry:
     """
     the empty geometry class
     """
-    def __init__(self, name="", value=1):
-        self.name = name
-        self.value = value
 
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        super().__init__()
         if type(self.value) != int:
             raise TypeError("{} must be an integer".format(self.name))
         if self.value <= 0:
