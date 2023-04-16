6ba7456d25b2ce33b7cf7c977be7c345ca3c5200
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-16 21:25:59 +0100

    Added scripts for 0x0A. Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,6 +9,9 @@ class MyList(list):
     """A class that inherits from
     the python builtin class of list
     """
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
         """
