03854333eb8e1da344344c0501aa1fd7c488abfb
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-18 12:21:41 +0100

    Added scripts for 0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+0x0B. Python - Input/Output
+Task 0 - reads a text file (UTF8) and prints it to stdout
+"""
+
+def read_file(filename=""):
+    with open(filename, encoding='UTF-8') as myfile:
+        fc = myfile.read()
+    print(fc)
