53af09c9d4244b66c6542c8da0370cf224ea025f
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-18 13:09:08 +0100

    Added scripts for 0x0B. Python - Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,5 +9,5 @@ def write_file(filename="", text=""):
     a function that writes a string to a text file (UTF8) and
     returns the number of characters written
     """
-    with open(filename, w, encoding="UTF-8") as ftw:
+    with open(filename, "w", encoding="UTF-8") as ftw:
         return ftw.write(text)
