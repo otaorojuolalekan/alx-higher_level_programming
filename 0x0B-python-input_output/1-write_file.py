40959793e8d19a48e147623e9d19a08bdaaba9a9
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-18 12:53:43 +0100

    Added scripts for 0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Task 2 scripts
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    a function that writes a string to a text file (UTF8) and
+    returns the number of characters written
+    """
+    with open(filename, w, encoding="UTF-8") as ftw:
+        return ftw.write(text)
