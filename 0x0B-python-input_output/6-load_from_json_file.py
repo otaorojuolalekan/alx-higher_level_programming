f8b239c25f11742660a5c6434ac16435aec59efc
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-19 05:36:18 +0100

    Added scripts for 0x0B. Python - Input/Output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,5 +11,5 @@ def load_from_json_file(filename):
     """
     This function creates an Object from a “JSON file”:
     """
-    with open(filename, "+r") as filestream:
+    with open(filename, "r") as filestream:
         return json.load(filestream)
