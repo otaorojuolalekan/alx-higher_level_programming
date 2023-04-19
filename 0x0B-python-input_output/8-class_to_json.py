978b435cf749c5c16a3040a9acbf46217e2c1afd
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-19 07:24:14 +0100

    Added scripts for 0x0B. Python - Input/Output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -15,3 +15,9 @@ def class_to_json(obj):
     for JSON serialization of an object
     """
     return (obj.__dict__)
+
+# if __name__ == "__main__":
+#     save_to_json_file = \
+#         __import__("5-save_to_json_file").save_to_json_file
+#     load_from_json_file = \
+#         __import__("6-load_from_json_file").load_from_json_file
