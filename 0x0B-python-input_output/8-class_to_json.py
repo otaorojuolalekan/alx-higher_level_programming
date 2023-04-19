6ad68f03f41a39e0e886de81e541fcae489ca98e
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-19 07:21:17 +0100

    Added scripts for 0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+0x0B. Python - Input/Output
+Task 8 - Write a function that returns the dictionary description
+with simple data structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object
+"""
+import sys
+
+
+def class_to_json(obj):
+    """
+    This function returns the dictionary description
+    with simple data structure (list, dictionary, string,
+    integer and boolean)
+    for JSON serialization of an object
+    """
+    return (obj.__dict__)
+
+# if __name__ == "__main__":
+#     save_to_json_file = \
+#         __import__("5-save_to_json_file").save_to_json_file
+#     load_from_json_file = \
+#         __import__("6-load_from_json_file").load_from_json_file
