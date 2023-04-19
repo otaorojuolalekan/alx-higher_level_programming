13b0bcb5b7325c928fc5d89d768328fe993d3e9c
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-04-19 06:23:49 +0100

    Added scripts for 0x0B. Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,26 +5,21 @@ Task 7 - Write a script that adds all arguments to a Python list,
 and then save them to a file
 """
 import sys
-import json
 
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = \
+        __import__("6-load_from_json_file").load_from_json_file
+    filename = "add_item.json"
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-filename = "add_item.json"
+    # load list_items from filename. if not exist return empty list
+    try:
+        list_items = load_from_json_file(filename)
+    except FileNotFoundError:
+        list_items = []
 
-# get the command line arguments as a list
-cmargs = sys.argv
+    # add command line arguments to list items
+    list_items.extend(sys.argv[1:])
 
-# convert the list to json and append it to add_item.json
-with open(filename, mode="a", encoding="UTF-8") as fp:
-    save_to_json_file(cmargs, filename)
-
-# load the json file content and remove sys.argv[0] i.e the scriptname
-with open(filename) as fp:
-    filecontent = load_from_json_file(filename)
-    filecontent.remove(sys.argv[0])
-
-# This time, overwrite filename with filecontent
-# (which does not have scriptname)
-with open(filename, mode="w", encoding="UTF-8") as fp:
-    save_to_json_file(filecontent, filename)
+    # add list items add_item.json file
+    save_to_json_file(list_items, filename)
