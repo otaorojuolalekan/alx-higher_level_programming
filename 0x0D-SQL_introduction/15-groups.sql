51a3282b7ca9f37c3d5cdaa0614910454e27a2b5
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-05-17 00:46:13 +0100

    TASK 15 FINAL UPDATE

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list number of records with the same score in table
-SELECT score,  COUNT(score) AS number FROM second_table GROUP BY 1 ORDER BY 2;
+SELECT score,  COUNT(score) AS number FROM second_table GROUP BY 1 ORDER BY 2 DESC;
