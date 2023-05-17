3c6053cb0d805e711a8a78a0dde7529e28455424
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-05-17 21:46:49 +0100

    another fix for task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,7 +6,6 @@ USE hbtn_0d_usa;
 
 CREATE TABLE
     IF NOT EXISTS states (
-        -- PRIMARY KEY(id),
         id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(256) NOT NULL
     );
