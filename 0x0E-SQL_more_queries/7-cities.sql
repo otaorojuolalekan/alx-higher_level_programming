f62e5beb375d8118177d29d9d6b11197e57354c2
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-05-17 21:50:21 +0100

    ANOTHER STYLE FOR TASK 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,6 @@ CREATE TABLE
     IF NOT EXISTS cities (
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
         state_id INT NOT NULL,
-        FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+        FOREIGN KEY(state_id) REFERENCES states(id),
         name VARCHAR(256)
     );
