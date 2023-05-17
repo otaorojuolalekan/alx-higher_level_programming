78a636b336ca5421e76ffe1ec2ccce5cbb237944
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-05-17 19:46:53 +0100

    fixed task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,6 +3,7 @@ CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE
     IF NOT EXISTS hbtn_0d_usa.states (
-        id INT UNIQUE NOT NULL PRIMARY KEY AUTO INCREMENT,
+        PRIMARY KEY(id),
+        id INT  AUTO INCREMENT,
         name VARCHAR(256) NOT NULL
     );
