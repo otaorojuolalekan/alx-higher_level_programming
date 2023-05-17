bd83fbf1dcc99ccdd9253d81b4a0227feea6551e
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-05-17 19:20:24 +0100

    attempts task 1 - 9

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- create databse hbtn_0d_usa and table states
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE
+    IF NOT EXISTS states (
+        id INT PRIMARY KEY AUTO INCREMENT,
+        name VARCHAR(256) NOT NULL
+    );
+    
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- cities table task 7
+CREATE DATABASE 
+    IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE
+    IF NOT EXISTS cities (
+        id INT PRIMARY KEY AUTO INCREMENT,
+        state_id INT NOT NULL,
+        FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+        name VARCHAR(256)
+    );
