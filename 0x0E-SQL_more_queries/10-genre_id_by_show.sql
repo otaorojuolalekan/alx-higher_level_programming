d19c030f1cc0a43fd6b18d264b28dfaf9791f206
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-05-17 22:22:23 +0100

    task 11

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,11 @@
 -- task 10
-SELECT s.title, g.genre_id
-  FROM tv_shows AS s
-        INNER JOIN tv_show_genres AS g
-	ON s.id = g.show_id
- ORDER BY s.title, g.genre_id;
+SELECT
+    s.title, g.genre_id
+FROM 
+    tv_shows AS s
+INNER JOIN 
+    tv_show_genres AS g
+ON 
+    s.id = g.show_id
+ ORDER BY 1, 2;
  
