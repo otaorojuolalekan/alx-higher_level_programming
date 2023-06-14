f2b1ab8a993ddabca68d3e9c0b022e045294265b
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-14 10:23:43 +0100

    task 4 reviewd

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const var1 = process.argv[2];
 const var2 = process.argv[3];
-sentence = var1 + ' is ' + var2;
-console.log(sentence);
+const sentence = var1 + ' is ' + var2;
+console.log(sentence); // iuhi
