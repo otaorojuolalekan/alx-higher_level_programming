e673275b3ab1fca41f50905de1aa9d37cc1e21ea
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-15 18:52:35 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const arg1 = Math.floor(Number(process.argv[2]));
-if (isNaN(arg1)) console.log('Not a number')
-else console.log(`My number: ${arg1}`)
+if (isNaN(arg1)) console.log('Not a number');
+else console.log(`My number: ${arg1}`);
