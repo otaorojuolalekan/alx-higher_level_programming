d69cf0ed5ffcd4db800b2098d9ad846b8c715aa4
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-16 12:48:48 +0100

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+function factorial (n) {
+  if (isNaN(n) || n === 1) {
+    return (1);
+  } else {
+    return (n * factorial(n - 1));
+  }
+}
+
+const arg1 = parseInt(process.argv[2]);
+console.log(factorial(arg1));
