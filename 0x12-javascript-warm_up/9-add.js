c64519e9baa4914708d6921c48b8d6731148d73a
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-16 12:27:12 +0100

    task 9-add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-function add(a, b) {
-    return parseInt(a) + parseInt(b);
-  }
-  
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
+}
+
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
 if (!arg1 || !arg2) console.log(NaN);
 else {
-const res = add(arg1, arg2);
-console.log(res);
+  const res = add(arg1, arg2);
+  console.log(res);
 }
