510186a1e37c9512c15ef522c94ffb517636ef2f
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-18 18:00:51 +0100

    task 0 final fix. added main function

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 """
 select states from db
-Usage: ./0-select_states.py user pass db
+Usage: ./0-select_states.py user pass
+
 """
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
+
+def main():
+    """
+    main function to ensure it doesn't
+    run on import"""
+
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("""SELECT * FROM states ORDER BY id ASC""")
     result = cur.fetchall()
     for res in result:
         print(res)
+
+
+if __name__ == '__main__':
+    main()
