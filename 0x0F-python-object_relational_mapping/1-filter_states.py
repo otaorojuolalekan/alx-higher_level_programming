e36501e7dcbca7e91741ac8f71d12a269c820375
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-18 18:13:23 +0100

    added script for task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+select states from db
+Usage: ./0-select_states.py user pass
+
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    main function to ensure it doesn't
+    run on import"""
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name like 'N%'""")
+    result = cur.fetchall()
+    for res in result:
+        print(res)
+
+
+if __name__ == '__main__':
+    main()
