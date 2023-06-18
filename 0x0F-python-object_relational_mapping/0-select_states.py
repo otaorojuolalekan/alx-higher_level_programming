65bcf7f4e211defa5fa9917e05a9471e2ccce591
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-18 17:50:26 +0100

    modified task 0 script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,19 +5,21 @@ select states from db
 import sys
 import MySQLdb
 
+
 def main():
     """
     main function to ensure it doesn't
     run on import"""
 
-    db=MySQLdb.connect(user=sys.argv[1],
-                       passwd=sys.argv[2],
-                       database=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
     cur = db.cursor()
     cur.execute("""SELECT * FROM states ORDER BY id ASC""")
     result = cur.fetchall()
     for res in result:
         print(res)
+
 
 if __name__ == '__main__':
     main()
