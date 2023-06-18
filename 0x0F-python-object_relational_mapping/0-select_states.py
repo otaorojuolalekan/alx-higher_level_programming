40ef805fe13ed24392c2029be1bf0629b0937eb0
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-18 17:57:18 +0100

    task 0 - modified script although previous works locally

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
 """
 select states from db
+Usage: ./0-select_states.py user pass db
+
 """
 import sys
 import MySQLdb
 
-
-def main():
-    """
-    main function to ensure it doesn't
-    run on import"""
-
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         database=sys.argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("""SELECT * FROM states ORDER BY id ASC""")
     result = cur.fetchall()
     for res in result:
         print(res)
-
-
-if __name__ == '__main__':
-    main()
