cd791a309b8a50ce687bc0f41eff32229875efd9
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-18 18:18:55 +0100

    added scripts for task 2 filter by user input

---
+++ 0x0F-python-object_relational_mapping/2-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+select states from db
+Usage: ./0-select_states.py user pass
+
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    main function to ensure it doesn't
+    run on import"""
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states""")
+    result = cur.fetchall()
+    for res in result:
+        if res[1] == sys.argv[4]:
+            print(res)
+
+
+if __name__ == '__main__':
+    main()
