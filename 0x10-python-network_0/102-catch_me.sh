836de3b21687ad446a81d0120a969c6ff043b62e
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-07-02 22:59:53 +0100

    task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request with a custom header to the catch_me endpoint
-curl -sX POST -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -sX PUT -L -d "user_id=98" --header "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
