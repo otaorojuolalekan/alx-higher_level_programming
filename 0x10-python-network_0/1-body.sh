841e1deb019cf4cd146f4c391886ff26a6814f0d
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-07-02 07:30:24 +0100

    solved task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # print body size # alt: curl -sfL $url
-curl -s -o /dev/null -w "%{response_code}\n" "$1" | [ $(cat) = "200" ] && curl -s -X "GET" "$1"
+curl -sL -o /dev/null -w "%{response_code}\n" "$1" | [ $(cat) = "200" ] && curl -sL -X "GET" "$1"
