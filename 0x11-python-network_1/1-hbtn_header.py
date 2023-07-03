801d23f1ae50a6afeedcb6937765c11f93636f45
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-07-03 05:51:23 +0100

    pycodestyle fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,13 +5,14 @@
 - of the X-Request-Id variable found in the header ofthe response.
 """
 import sys
-import urllib.request
+import urllib.request as ulb
+
 
 def main():
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
+    request = ulb.Request(url)
+    with ulb.urlopen(request) as response:
         print(dict(response.headers).get("X-Request-Id"))
 
 
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,15 +6,19 @@
 - displays the body of the response
 """
 import sys
-import urllib.parse
-import urllib.request
+import urllib.parse as ulp
+import urllib.request as ulb
+
+
+def main():
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = ulp.urlencode(value).encode("ascii")
+
+    request = ulb.Request(url, data)
+    with ulb.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    main()
